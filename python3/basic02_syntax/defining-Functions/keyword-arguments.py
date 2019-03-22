e9bd0216479131cf46de21b8832e99bdb60516d3
Your Name <you@example.com>
2019-03-22 18:37:39 +0800

    add keyword-arguments.py

--- python3/basic02_syntax/defining-Functions/keyword-arguments.py
+++ python3/basic02_syntax/defining-Functions/keyword-arguments.py
@@ -11,5 +11,18 @@ sum(b=2, a=1)
 sum(*[1, 2])
 sum(**{'a': 1, 'b': 2})
 
+def f(kind, *arguments, **keywords):
+    print(kind)
+    for index in arguments:
+        print(index)
+    for key in keywords:
+        print(key, '---', keywords[key])
+
+f('python', 1, 2, 3, 4, username='root', password='1234', host='127.0.0.1')
+f('python', *(1, 2, 3, 4), **{'username': 'root', 'password': '1234', 'host': '127.0.0.1'})
+
+
+
+
 
 
