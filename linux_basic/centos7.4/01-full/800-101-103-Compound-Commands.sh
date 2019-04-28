96c1cf90ec9456ddb0d7207da8366fcc7f7c3c04
Your Name <you@example.com>
2019-04-28 09:19:34 +0800

    add 800-101-103-Compound-Commands.sh

--- linux_basic/centos7.4/01-full/800-101-103-Compound-Commands.sh
+++ linux_basic/centos7.4/01-full/800-101-103-Compound-Commands.sh
@@ -29,10 +29,52 @@
 }
 
 
+#// ((expression))
+#//      根据man bash文档中 ARITHMETIC EVALUATION 部分描述的规则 对 expression 进行 运算求值, 如果 expression 的值 为 非0 (non-zero), 则 return status 为 0,
+#//      否则 return status 为 1, 这完全等价于 let "expression"
+
+
+((2+3))
+echo $?
+#// 0
+((2-2))
+echo $?
+#// 1
 
 
 
-
-
+#// [[ expression ]]
+#//       根据条件表达式 expression 返回 0 或 1 的 status, Expressions 由man bash 帮助页中 CONDITIONAL EXPRESSIONS 部分描述的基础条件表达式组成。在
+#//       [[ 和 ]] 之间是不会执行 Word splitting 和 pathname expansion 的, 但是 tilde expansion, parameter and variable expansion,
+#//       arithmetic  expansion, command substitution, process substitution, and quote removal 依然会被执行. 如 -f 这样的条件操作符(Conditional operators)
+#//       不能加引号以使其被视为条件表达式的基本构成成分.
+#//
+#//       当使用 [[ 时， 操作符 < 和 > 使用当前的 locale 来做 字典顺序排序(sort lexicographically)
+#//
+#//       当操作符 == 和 != 被使用时, 这些操作符右边的 string 会被视为 pattern 并 按照接下来 描述的 Pattern Matching 的规则进行匹配(match)操作。
+#//       如果 shell 的 nocasematch 选项被启用, 执行匹配操作时会 会忽略 字母(alphabetic characters)的大小写情况，
+#//           操作符 == 在pattern匹配成功时返回 0, 操作符 != 在 pattern 匹配失败时 返回 0.
+#//           反之则返回 1(即操作符 == 在 不匹配时返回 1 ，操作符 != 在成功匹配时返回 1 )。
+#//           pattern 的任何部分 都 可以被引号引起来 使其被强制作为 string 来被匹配。
+#//
+#//       额外的, 二元操作符 =~ 也是 可用的，操作符 =~ 具有与 == 和 != 相同的优先级. 当操作符 =~ 被使用时, 其右边的 string 被视为
+#//       扩展的正则表达式extended  regular  expression 并根据 regex(3) (见 man 3 regex 手册页)那样来匹配, 如果匹配成功，则返回 0，
+#//       反之匹配失败则返回 1，如果该正则表达式本身 语法上 就存在错误，则 该条件表达式 的返回值 就为 2. 如果 shell 的 nocasematch 选项
+#//       被启用, 则 在做匹配操作的时候会忽略 字母(alphabetic  characters) 的大小写情况. pattern 的任何部分 都可以被引号引起来 使其 被
+#//       强制作为 string 来被匹配. 字符串中与正则表达式中的 parenthesized subexpressions 匹配成功的 Substrings
+#//       会被保存在数组变量 BASH_REMATCH 中， 数组变量 BASH_REMATCH 中 索引 为 0 对应的
+#//       元素 是字符串 中 与完整的正则表达式(entire  regular expression)匹配成功的部分.
+#//       BASH_REMATCH 中 索引为 n 的元素 是匹配第n个 parenthesized subexpression 匹配成功的部分.
+#//       Expressions 可以 使用如下的操作符 连接 起来(它们的优先级按从上到下的顺序依次递减):
+#//               ( expression )
+#//                      Returns the value of expression.  This may be used to override the normal precedence of operators.
+#//               ! expression
+#//                      True if expression is false.
+#//               expression1 && expression2
+#//                      True if both expression1 and expression2 are true.
+#//               expression1 || expression2
+#//                      True if either expression1 or expression2 is true.
+#//
+#//               The && and || operators do not evaluate expression2 if the value of expression1 is sufficient to determine the return value of the entire conditional expression.
 
 
