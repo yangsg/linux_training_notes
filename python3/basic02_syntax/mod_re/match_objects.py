b26b40178b45b45c6baaa2cfdf659e431f9b219d
Your Name <you@example.com>
2019-07-28 11:35:50 +0800

    update match_objects.py

--- python3/basic02_syntax/mod_re/match_objects.py
+++ python3/basic02_syntax/mod_re/match_objects.py
@@ -1,2 +1,17 @@
 #// https://docs.python.org/3.6/library/re.html#match-objects
 import re
+
+
+# 注: python 中的 正则表达式 \d 和 [0-9] 并非总是等价, 这与编码有关系,
+#     如果是仅匹配 0 到 9 之间的 数字, 最好使用 [0-9] 这种方式, 而不要使用
+#     \d 这种方式
+
+m = re.search(r'\d+', '٣٤٥۳')  # 不推荐使用 \d , 而最好使用 [0-9]
+print(m.group(0))  # 输出: ٣٤٥۳,  所以如果没有指定 ASCII flag, \d 还会匹配 unicode 中 Nd 类别的其他字符.
+                   # http://www.fileformat.info/info/unicode/category/Nd/list.htm
+                   # https://docs.python.org/3.6/library/re.html
+                   # https://docs.python.org/3.6/howto/regex.html
+
+
+
+
